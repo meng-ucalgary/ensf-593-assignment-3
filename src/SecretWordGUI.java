8c4ba18c018cf820ba9e05d1e5f294007aecb786
Bhavyai Gupta <zbhavyai@gmail.com>
2021-05-28 17:18:47 +0530

    added Javadoc for the GUI

--- src/SecretWordGUI.java
+++ src/SecretWordGUI.java
@@ -1,68 +1,101 @@
-import javax.swing.*; 
+import javax.swing.*;
 import java.awt.*;
-import java .awt. event .*;
-	
-/**
- * @author 
- *
- */
-public class SecretWordGUI extends JFrame implements ActionListener{
+import java.awt.event.*;
 
-	private JTextField inputField; 
-	private JTextArea display;
+/**
+ * Class to provide a GUI interface that lets the user make repeated guesses at
+ * the letters contained in a secret word. Each time the user makes a guess, the
+ * GUI reports whether the guess is correct or not and displays the updated
+ * version of the secret word.
+ *
+ * @author Bhavyai Gupta
+ * @version 1.0
+ * @since May 28, 2021
+ */
+public class SecretWordGUI extends JFrame implements ActionListener {
+    /**
+     * The input field to fetch the guess made by the user
+     */
+    private JTextField inputField;
+
+    /**
+     * Display area to print if the guess made by the user was correct or wrong, along
+     * with the updated version of the secret word to be guessed
+     */
+    private JTextArea display;
+
+    /**
+     * The String to store the user guess fetched from the inputField
+     */
     private String inputString;
+
+    /**
+     * variable of type SecretWord, which holds the logic of this program and
+     * provides instance methods of SecretWord
+     */
     private SecretWord secret;
 
-	public SecretWordGUI(String title)  {
-		secret = new SecretWord();
-		
-		JLabel prompt = new JLabel("Input a single character to guess the secret word (hit ENTER):");
+    /**
+     * Constructs the GUI interface to interact with the user
+     *
+     * @param title The title to show on the title bar of the windows
+     */
+    public SecretWordGUI(String title) {
+        secret = new SecretWord();
 
-		inputField = new JTextField(5); 
-		inputField.setText("");
-		inputField.addActionListener( this ); 
+        JLabel prompt = new JLabel("Input a single character to guess the secret word (hit ENTER):");
 
-		display = new JTextArea (10 ,30);
-		display.append("The secret word: " + secret.getDisplayedWord() +"\n");
+        inputField = new JTextField(5);
+        inputField.setText("");
+        inputField.addActionListener(this);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(0,2));
-		panel.add(prompt);
-		panel.add(inputField);
+        display = new JTextArea(10, 30);
+        display.append("The secret word: " + secret.getDisplayedWord() + "\n");
 
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new BorderLayout());
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0, 2));
+        panel.add(prompt);
+        panel.add(inputField);
 
-		contentPane.add("Center", panel);
-		contentPane.add("South", display);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
 
-		setTitle(title);             
-		pack();
-		setVisible(true);
+        contentPane.add("Center", panel);
+        contentPane.add("South", display);
 
-	}
+        setTitle(title);
+        pack();
+        setVisible(true);
 
-	public void actionPerformed(ActionEvent evt)  { 
-		if ( evt.getSource() == inputField) { 
-			inputString = inputField.getText();
-	        inputField.setText("");
-	        boolean guess = secret.makeGuess(inputString.charAt(0));
+    }
 
-	                            // Process the user's guess
-	        if (guess == true) 
-	            display.append ("That Guess Was Right " + secret.getDisplayedWord() + "\n");
-	        else 
-	            display.append ("That Guess Was Wrong " + secret.getDisplayedWord() + "\n");
+    /**
+     * Catches the events by the ActionListener as a result of user interaction with
+     * the GUI interface
+     *
+     * @param evt The action caught by the GUI
+     */
+    public void actionPerformed(ActionEvent evt) {
+        if (evt.getSource() == inputField) {
+            inputString = inputField.getText();
+            inputField.setText("");
+            boolean guess = secret.makeGuess(inputString.charAt(0));
 
-		}
-	}
+            // Process the user's guess
+            if (guess == true)
+                display.append("That Guess Was Right " + secret.getDisplayedWord() + "\n");
+            else
+                display.append("That Guess Was Wrong " + secret.getDisplayedWord() + "\n");
+        }
+    }
 
-	public static void main(String[] args) {
-
-		//Starting the GUI application
-		new SecretWordGUI("Guess the secret word");
-
-	}
-
+    /**
+     * Method to create the GUI by calling the constructor SecretWordGUI
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        // Starting the GUI application
+        new SecretWordGUI("Guess the secret word");
+    }
 }
-
